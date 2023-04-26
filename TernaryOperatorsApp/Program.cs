53d4c7809195219c183209fda6346bea9baa262d
msimangoksi@gmail.com <msimangoksi@gmail.com>
2023-04-26 09:05:00 +0200

    Add project files.

---
+++ TernaryOperatorsApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Msimago KSI
+namespace TernaryOperatorsApp
+{
+    class Program
+    {
+        //ternary operator in action using C#
+        static void Main(string[] args)
+        {
+            //first
+            string color = "blue";
+            string result = (color == "blue") ? "blue" : "NOT blue";
+
+            Console.WriteLine(result);
+
+            //Second
+            int pepperLength = 2;
+            string message = (pepperLength >= 3.5) ? "ready!" : "wait a little longer";
+
+            Console.WriteLine(message);
+        }
+    }
+}
